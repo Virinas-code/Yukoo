488e669889fdd01047f196a8347cae983c72cda0
Virinas-code <alexcode228@gmail.com>
2021-02-24 21:57:58 +0100

    Fix lint

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python3
 """Test file."""
+import sys
+err_code = 0
+
 try:
     import my_engine
     print("✅ Import du module")
-except Exception as e:
-    print(f"❎ Import du module : {e}")
+except Exception as exception:
+    print(f"❎ Import du module : {exception}")
+    err_code -= 1
 
 try:
     test_engine = my_engine.EngineBase("Test", "Yukoo test")
     print("✅ EngineBase")
-except Exception as e:
-    print(f"❎ EngineBase : {e}")
+except Exception as exception:
+    print(f"❎ EngineBase : {exception}")
+    err_code -= 1
+
+sys.exit(err_code)
