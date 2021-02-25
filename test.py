2016f35defa455e813e466db2825eae527bc3693
Virinas-code <alexcode228@gmail.com>
2021-02-25 18:24:59 +0100

    Better test : no f string for Python 2

--- test.py
+++ test.py
@@ -8,14 +8,14 @@ try:
     import my_engine
     print("✅ Import du module")
 except Exception as exception:
-    print(f"❎ Import du module : {exception}")
+    print("❎ Import du module :", exception)
     err_code -= 1
 
 try:
     test_engine = my_engine.EngineBase("Test", "Yukoo test")
     print("✅ EngineBase")
 except Exception as exception:
-    print(f"❎ EngineBase : {exception}")
+    print("❎ EngineBase :", exception)
     err_code -= 1
 
 
@@ -23,9 +23,9 @@ def test(cmd, name):
     global err_code
     try:
         exec(cmd, globals(), locals())
-        print(f"✅ {name}")
+        print("✅", name)
     except Exception as exception:
-        print(f"❎ {name} : {exception}")
+        print("❎", name, ":", exception)
         err_code -= 1
 
 
